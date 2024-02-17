7dc6bc8efca863b9bc81c3c28f20ce18803adb58
tangevelyn22 <tangevelyn22@gmail.com>
2024-02-17 15:00:13 -0600

    StudentAdminSystem: initial setup

---
+++ StudentAdminSystemProject/src/main/java/com/studentAdminProject/StudentAdminSystemProject/StudentAdminSystemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.studentAdminProject.StudentAdminSystemProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAdminSystemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAdminSystemProjectApplication.class, args);
+	}
+
+}
