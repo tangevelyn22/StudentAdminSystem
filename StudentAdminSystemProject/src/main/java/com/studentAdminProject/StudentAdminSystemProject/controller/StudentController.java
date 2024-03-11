7ab22cadcc43f0d66eb70ae3ae4793e8b91b8594
tangevelyn22 <tangevelyn22@gmail.com>
2024-03-10 21:01:14 -0500

    AGILE: save write to table

--- StudentAdminSystemProject/src/main/java/com/studentAdminProject/StudentAdminSystemProject/controller/StudentController.java
+++ StudentAdminSystemProject/src/main/java/com/studentAdminProject/StudentAdminSystemProject/controller/StudentController.java
@@ -1,4 +1,26 @@
 package com.studentAdminProject.StudentAdminSystemProject.controller;
 
+import com.studentAdminProject.StudentAdminSystemProject.model.Student;
+import com.studentAdminProject.StudentAdminSystemProject.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+
+    @PostMapping("/addStudent")
+    public boolean addAction(@RequestBody Student student) {
+        Student test = this.studentService.createStudent(student);
+        return true;
+    }
+
+    @GetMapping("./getStudent")
+    public boolean getStudent(){
+        return false;
+    }
 }
--- StudentAdminSystemProject/src/main/java/com/studentAdminProject/StudentAdminSystemProject/service/StudentServiceInterface.java
+++ StudentAdminSystemProject/src/main/java/com/studentAdminProject/StudentAdminSystemProject/service/StudentServiceInterface.java
@@ -1,4 +1,9 @@
 package com.studentAdminProject.StudentAdminSystemProject.service;
 
+import com.studentAdminProject.StudentAdminSystemProject.model.Student;
+import java.util.List;
+
 public interface StudentServiceInterface {
+    public Student createStudent(Student student);
+    public List<Student> getStudents();
 }
